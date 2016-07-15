fdd295cc2ede475bbc16ae5a7f1bca19793cfba5
boris <boris@DESKTOP-IICH6JK>
2016-07-15 13:19:23 +0900

    전체 커밋

---
+++ src/main/java/eppn/sample/service/SampleService.java
@@ -0,0 +1,13 @@
+package eppn.sample.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface SampleService {
+	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
+
+	void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+}
